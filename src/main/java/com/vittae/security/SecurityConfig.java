ffe6c179a7579d85d70825a2ccf1fa1850948fc6
GustavoGomes <ggaprado@gmail.com>
2026-05-22 22:36:09 -0300

    alteracoes apenas no cors, securityChain

--- src/main/java/com/vittae/security/SecurityConfig.java
+++ src/main/java/com/vittae/security/SecurityConfig.java
@@ -1,19 +1,22 @@
 package com.vittae.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.vittae.service.UsuarioService;
 
@@ -33,9 +36,8 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(
-	        AuthenticationConfiguration config) throws Exception {
-	    return config.getAuthenticationManager();
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
 	}
 
 	@Bean
@@ -44,16 +46,28 @@ public class SecurityConfig {
 				.sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(req -> {
 					req.requestMatchers(HttpMethod.GET, "/api/medicos").permitAll();
-					
+
 					req.requestMatchers(HttpMethod.POST, "/api/medicos").permitAll();
-					
+
 					req.requestMatchers(HttpMethod.POST, "/api/login").permitAll();
-					
+
 					req.requestMatchers(HttpMethod.POST, "/api/agendamentos").permitAll();
-					
+
 					req.requestMatchers(HttpMethod.GET, "/api/medicos/*/horarios-livres").permitAll();
 
 					req.anyRequest().authenticated();
 				}).build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(List.of("http://localhost:8081"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		config.setAllowedHeaders(List.of("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 }
